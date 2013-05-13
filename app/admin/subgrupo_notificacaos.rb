6feac54450422b6ead48f079dc5dbb8d0130db75
Ulisses Constantini <torquatro@gmail.com>
2013-05-13 09:39:42 -0300

    removido models não utilizados

--- app/admin/subgrupo_notificacaos.rb
+++
@@ -1,4 +0,0 @@
-# -*- encoding : utf-8 -*-
-ActiveAdmin.register SubgrupoNotificacao do
-	menu :label => "Subgrupo de Notificação"
-end
--- app/models/subgrupo_notificacao.rb
+++
@@ -1,9 +0,0 @@
-# -*- encoding : utf-8 -*-
-class SubgrupoNotificacao < ActiveRecord::Base
-  attr_accessible :descricao, :identificador
-
-  validates :identificador, :uniqueness => true
-  def name
-  	"#{self.descricao} | #{self.identificador}"
-  end
-end
