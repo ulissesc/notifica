c5e85fc87831a2c6a561e571f9a71f25b6d1e563
Ulisses Constantini <torquatro@gmail.com>
2013-05-13 09:58:04 -0300

    update dos elementos do banco

---
+++ db/migrate/20130513125129_add_account_to_elements.rb
@@ -0,0 +1,12 @@
+class AddAccountToElements < ActiveRecord::Migration
+  def change
+
+  	Account.create(:label => "CONTEXTO TESTE")
+  	
+  	Destinatario.update_all(:account_id => Account.first.id)
+  	Notificacao.update_all(:account_id => Account.first.id)
+	Visualizacao.update_all(:account_id => Account.first.id)
+  	GrupoNotificacao.update_all(:account_id => Account.first.id)
+  	AdminUser.update_all(:account_id => Account.first.id)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-# -*- encoding : utf-8 -*-
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130513123628) do
+ActiveRecord::Schema.define(:version => 20130513125129) do
 
   create_table "accounts", :force => true do |t|
     t.string   "label"
