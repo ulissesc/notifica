389c1c69ffbd10336a588ff523b61691b32b37c5
ulissesc <torquatro@gmail.com>
2013-05-17 11:38:04 -0300

    melhorias

--- app/admin/grupo_notificacaos.rb
+++ app/admin/grupo_notificacaos.rb
@@ -1,4 +1,12 @@
 # -*- encoding : utf-8 -*-
 ActiveAdmin.register GrupoNotificacao do
   menu :label => "Grupo de Notificação"
+
+  form do |f|                         
+    f.inputs "Grupo de Notificações" do       
+      f.input :descricao                  
+      f.input :identificador               
+    end                               
+    f.actions                         
+  end  
 end
--- app/admin/visualizacoes.rb
+++ app/admin/visualizacoes.rb
@@ -1,4 +1,13 @@
 # -*- encoding : utf-8 -*-
 ActiveAdmin.register Visualizacao do
 	menu :label => "Visualizações"  
+
+	form do |f|                         
+	    f.inputs "Visualizações" do       
+	      f.input :notificacao                  
+	      f.input :data_hora
+	      f.input :visto_por               
+	    end                               
+	    f.actions                         
+    end  
 end
